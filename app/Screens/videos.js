1ccd56d0b173d3d26fc2e822eb3b05e7d9b0d369
temapalesa <temapalesa@gmail.com>
2018-10-04 10:57:14 +0200

    updated videos

--- app/Screens/videos.js
+++ app/Screens/videos.js
@@ -1,29 +1,88 @@
 import React, { Component } from 'react';
-import {View, Text , Dimensions,StyleSheet ,Image,  } from 'react-native';
+import {
+    View,
+    Text ,
+    Dimensions,
+    StyleSheet,
+    TouchableHighlight ,
+    Image ,
+    ActivityIndicator 
+ } from 'react-native';
 import {WebView} from 'react-native';
-import Spinner from 'react-native-loading-spinner-overlay';
+// import Spinner from 'react-native-loading-spinner-overlay';
 import { Container, Content, CardItem ,Card } from 'native-base';
-import Header from '../components/HeaderComponent';
-
-
+import HeaderComponent from '../components/HeaderComponent';
 
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
 
 
-class Video extends Component{
-     render(){
-         return(
+class Videos extends Component{
 
-            <View>
-            <Header {...this.props} />
-            <Content style={{backgroundColor: '#fff'}}>
-
-            </Content>
-
-
-            </View>
-         )
-     }
+    ActivityIndicatorLoadingView(){
+        return (
+            <ActivityIndicator
+                color='#009688'
+                size='large'
+                style={styles.ActivityIndicatorStyle}
+            />
+        );
     }
-export default Video;
+     
+    render() {
+        return (
+        //  <ActivityIndicator/>
+            <Container>
+                  <View style={{backgroundColor:'white'}}>
+            <HeaderComponent {...this.props}/>
+            
+           <View >
+        
+        </View>
+        </View>
+                <Content>
+                    <Card style={{width: 1000}}>
+                       
+                            <WebView
+                                source={{uri:'https://www.youtube.com/embed/PGUMRVowdv8'}}
+                                style={styles.WebViewStyle}
+                                javaScriptEnabled={true}
+                                domStorageEnabled={true}
+                                renderLoading={this.ActivityIndicatorLoadingView}
+                                startInLoadingState={true}
+                            />
+                       
+                    </Card>
+               
+                </Content>
+            </Container>
+         
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+   
+
+       WebViewStyle : {
+            justifyContent : 'center',
+            alignItems : 'center',
+            flex : 1,
+            marginTop : 20  ,
+            width : 500 ,
+            height : 150 
+       },
+       ActivityIndicatorStyle : {
+            position : 'absolute',
+            left : 0 , 
+            right : 0 ,
+            top : 0, 
+            bottom : 0 ,
+            alignItems : 'center',
+            justifyContent : 'center',
+
+       }
+});
+
+export default Videos;
