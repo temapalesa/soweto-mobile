c1f07f16fe22673109258857e014a3e691be8287
temapalesa <temapalesa@gmail.com>
2018-10-10 16:39:21 +0200

    ActivityIndicator to a video

--- app/Screens/videos.js
+++ app/Screens/videos.js
@@ -6,7 +6,8 @@ import {
     StyleSheet,
     TouchableHighlight ,
     Image ,
-    ActivityIndicator 
+    ActivityIndicator , 
+    Modal
  } from 'react-native';
 import {WebView} from 'react-native';
 // import Spinner from 'react-native-loading-spinner-overlay';
@@ -19,19 +20,30 @@ const deviceWidth = Dimensions.get('window').width;
 
 class Videos extends Component{
 
-    ActivityIndicatorLoadingView(){
-        return (
-            <ActivityIndicator
-                color='red'
-                size='large'
-                style={styles.ActivityIndicatorStyle}
-            />
-        );
-    }
-     
+            ActivityIndicatorLoadingView(){
+                return(
+                    <ActivityIndicator
+                        size = 'large'
+                        color = '#ff69b4'
+                        style = {styles.indicator}
+                    />
+                )
+            }
+            
+            reload = () => {
+                this.setState({
+                  viewState: WebViewState.LOADING
+                });
+                UIManager.dispatchViewManagerCommand(
+                  this.getWebViewHandle(),
+                  UIManager.RCTWebView.Commands.reload,
+                  renderLoading
+                 );
+               };
+
+            
     render() {
         return (
-        //  <ActivityIndicator/>
             <Container>
                   <View style={{backgroundColor:'white'}}>
             <HeaderComponent {...this.props}/>
@@ -40,28 +52,58 @@ class Videos extends Component{
         
         </View>
         </View>
+        
                 <Content>
-                    <Card style={{width: 1000}}>
-                       
+                    <Card      style = {{width : 400, height : 280}}>
                             <WebView
-                                source={{uri:'https://www.youtube.com/watch?v=jv-hIAYG6KM'}}
-                                style={styles.WebViewStyle}
-                                startInLoadingState ={true}
-                                renderLoading={this.ActivityIndicatorLoadingView}
-                                geolocationEnabled={true}
+                            style = {{width : 400, height : 250}}
+                                source={{uri:'https://youtu.be/eLPrJ77X_Vk'}}
+                                domStorageEnabled={true}
+                               mediaPlaybackRequiresUserAction={true}
+                                scalesPageToFit={true}
+                               javaScriptEnabled={true}
+                                renderLoading={this.props.ActivityIndicatorLoadingView}
+                                startInLoadingState={true}
+                                ignoreSslError={true} 
                             />
-                        <CardItem>
+                        <CardItem  style = {{width : 400}}>
                             <Image
                                  source={require('../Images/download.png')}
-                                style={{width:50 , height: 50 , borderRadius: 30}}
+                                style={{width:70 , height: 70 , borderRadius: 30}}
                             />
-                             <Text style={{flex : 1}}> Parts of the Bara taxi rank in Soweto have been closed for the time being.
+                            <View>
+                            <Text style={{fontWeight : 'bold'}}>Title</Text>
+                             <Text > Parts of the Bara taxi rank in Soweto have been closed for the time being.
                                     This after a shoot out between two rival taxi associations...
                                   Scores of commuters were left stranded as police fear a taxi war may be brewing.</Text>
-                                 
+                                  </View>
                             </CardItem>
                     </Card>
-               
+                    <Card      style = {{width : 400, height : 280}}>
+                            <WebView
+                            style = {{width : 400, height : 250}}
+                                source={{uri:'https://youtu.be/eLPrJ77X_Vk'}}
+                                domStorageEnabled={true}
+                               mediaPlaybackRequiresUserAction={true}
+                                scalesPageToFit={true}
+                               javaScriptEnabled={true}
+                                renderLoading={this.props.ActivityIndicatorLoadingView}
+                                startInLoadingState={true}
+                                ignoreSslError={true} 
+                            />
+                        <CardItem  style = {{width : 400}}>
+                            <Image
+                                 source={require('../Images/download.png')}
+                                style={{width:70 , height: 70 , borderRadius: 30}}
+                            />
+                            <View>
+                            <Text style={{fontWeight : 'bold'}}>Title</Text>
+                             <Text > Parts of the Bara taxi rank in Soweto have been closed for the time being.
+                                    This after a shoot out between two rival taxi associations...
+                                  Scores of commuters were left stranded as police fear a taxi war may be brewing.</Text>
+                                  </View>
+                            </CardItem>
+                    </Card>
                 </Content>
             </Container>
          
@@ -70,27 +112,15 @@ class Videos extends Component{
     }
 }
 
+
 const styles = StyleSheet.create({
    
-
-       WebViewStyle : {
-            justifyContent : 'center',
-            alignItems : 'center',
-            flex : 1,
-            marginTop : 20  ,
-            width : 500 ,
-            height : 300 ,  
-       },
-       ActivityIndicatorStyle : {
-            position : 'absolute',
-            left : 0 , 
-            right : 0 ,
-            top : 0, 
-            bottom : 0 ,
+        indicator : {
+            flex : 1 ,
             alignItems : 'center',
             justifyContent : 'center',
-
-       }
+            height :  80 
+        }
 });
 
 export default Videos;
