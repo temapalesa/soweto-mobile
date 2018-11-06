49d8d8a9f15db8d40c6d20c8baa7602ede45e051
lindiwe095@gmail.com <lindiwe095@gmail.com>
2018-11-06 14:14:14 +0200

    work

--- app/components/AboutComponent.js
+++ app/components/AboutComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 import {Icon} from 'react-native-elements'
 
 import HeaderComponent from '../components/HeaderComponent';
@@ -20,31 +20,32 @@ class AboutComponent extends Component {
         return (
             <View>
                 <HeaderComponent />
-                <View style={{ justifyContent:'center'}}>
+                <View >
 
-                    <Text style={{ fontWeight: 'bold', fontSize: 22, marginTop: 10, alignItems:'center'}}>- BACKGROUND -</Text>
-                    <Text style={{ fontSize: 15, marginRight: 5, marginLeft: 5, alignItems:'center' }}>
+                    <Text style={styles.TextHearder}>BACKGROUND </Text>
+
+                    <Text style={styles.TextBody}>
                         Soweto Observer is a local community newspaper that cover news in Soweto.
                          The newspaper is 100% owned by The Genial Media Group. Its founders are young people from different  walk of life.
                          It started its print publication in August 2017 which is running to date
                </Text>
-                    <Text style={{ fontWeight: 'bold', fontSize: 22,justifyContent:'center' }}> - Mission and Vision -</Text>
+                    <Text style={styles.TextHearder}> Mission and Vision </Text>
 
-                    <Text style={{ fontSize: 15, marginRight: 5, marginLeft: 5, justifyContent:'center' }}> We aim serve our community with news that are truthful,
+                    <Text style={styles.TextBody}> We aim serve our community with news that are truthful,
                          objective and fair. Soweto observer is not affilated to any political party.
                           We uphold high standard journalism.//#endregion.//#endregion
                         We hope to reach out to the youth and the community as a whole
              </Text>
            
-             <Text style={{ fontWeight: 'bold', fontSize: 22,justifyContent:'center' }}>- Contact Details -</Text>
+             <Text style={styles.TextHearder}>Contact Details</Text>
               
-                <Text style={{ fontSize: 15 }}> 081 7595 432</Text>
+                <Text style={styles.InfoText}> Call: 081 7595 432</Text>
 
-                <Text style={{ fontSize: 15 }}>  063 451 5871</Text>
+                <Text style={styles.InfoText}>  Call: 063 451 5871</Text>
 
-                 <Text style={{ fontSize: 15 }}>info@sowetoobserver.co.za </Text>
+                 <Text style={styles.InfoText}> Email: info@sowetoobserver.co.za </Text>
 
-                  <Text style={{ fontSize: 15, }}> Robert@sowetoobserver.co.za</Text>
+                  <Text style={styles.InfoText}> Email: Robert@sowetoobserver.co.za</Text>
 
            
      </View>
@@ -57,6 +58,28 @@ class AboutComponent extends Component {
 
 
 }
+const styles=StyleSheet.create({
+
+    TextHearder:{
+         fontWeight: 'bold',
+         fontSize: 22,
+         marginTop: 14,
+         alignSelf:'center'
+    },
+    TextBody:{
+         fontSize: 15,  
+         alignItems:'center',
+         marginTop:10,
+         flexWrap : 'wrap',
+         paddingLeft:6,
+         paddingRight:6,
+    },
+    InfoText:{
+        fontSize: 15,
+        alignSelf:'center',
+        marginTop:10,
+    },
+})
 
 
 export default AboutComponent;
--- app/components/PoloticsComponent.js
+++ app/components/PoloticsComponent.js
@@ -1,47 +1,92 @@
 import React, { Component } from 'react';
 import { ScrollView, View, Text, Image,Dimensions, StyleSheet, Tile, TouchableHighlight,TouchableOpacity} from 'react-native';
-
-import { Container, Header, Content,Right, Card, CardItem, Thumbnail,  Button, Icon, Left, Body } from 'native-base';
-
+import { connect } from 'react-redux';
+import * as actions from '../src/actions';
 import HeaderComponent from './HeaderComponent';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards'
 
 class Polotics extends Component {
+
     static navigationOptions = ({navigation}) => { 
-        let drawerLabel ='Polotics' ;
+        let drawerLabel ='Politics' ;
+     
      
         return { drawerLabel};
         
     }
+    componentDidMount(){
+        this.props.fetchArticles();
+    }
+    viewArticles(id){
+        this.props.fetchArticles(id)
+    }
+
       render() {
+
+        const { articles } = this.props; 
 
         return (
 <View>
     <HeaderComponent />
-    
-               
-               <View >
-               <TouchableHighlight style ={{ 
-                width: 125,
-                margin: 6,
-                height: 30,
-                backgroundColor: '#59cbbd',
-                alignSelf: 'center',
-            
-            }}
-            onPress={() =>{
-                const{navigate} = this.props.navigation;
-                navigate('');
 
-            }}>
-            <Text style= {{color:'white', fontSize: 22, alignSelf: 'center',  }}> Polotics</Text>
+            <ScrollView>
+            {!articles ? <ActivityIndicator size="large" color="#0097A7" hidesWhenStopped={true} />
+                        : articles.map((data, index) => {
 
-            </TouchableHighlight>
-            </View>
-           
+                        if(data.category.name == "politics"){
+                                
+                            return (
+
+                                <Content key={index} style={{ backgroundColor: '#fff' }}>
+
+                                    <View key={index} onPress={() => this.article(data._id)}>
+
+
+                                        <Card style={{ height: 250, marginTop: 10, marginLeft: 5, marginRight: 5 }} onPress={() => this.article(data._id)}>
+
+                                            <CardItem>
+
+                                                <Body>
+
+                                                    <TouchableOpacity onPress={() => this.article(data._id)}>
+                                                        <Thumbnail square source={{ uri: data.picture }} style={{ height: 150, width: 380, }} />
+                                                        <Text style={{ fontFamily: "SEGIO UI", paddingTop: 20, justifyContent: 'center', color: 'black', marginLeft: 25, fontSize: 18 }} >{data.title}
+                                                        </Text>
+                                                    </TouchableOpacity>
+                                                </Body>
+
+                                            </CardItem>
+                                        </Card>
+
+
+                                    </View>
+
+                                </Content>
+
+                            )
+                            }
+
+                        })   
+
+                        
+                    }
+
+                
+
+
+
+                
+                
+                </ScrollView>           
 </View>
               
         )   
     }
 }
+function mapStateToProps({ articles }) {
+    return {
+        articles
+    }
+}
 
-export default Polotics;
+export default connect(mapStateToProps, actions)(Polotics);
